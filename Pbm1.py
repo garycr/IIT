3d28f1be3e0d286238daa94634c212ceb365f9ad
Gary Crossland <garycr@microsoft.com>
2021-04-11 16:02:44 -0500

    Updated to remove multiplication

--- Pbm1.py
+++ Pbm1.py
@@ -1,26 +1,18 @@
 import numpy as np
 
-E = ([[0,1,0],[0,0,1],[1,1,0]])
-E2 = ([[0,1,0],[0,0,1],[1,1,0]])
-
+E = ([[0,1,0],[1,0,1],[1,1,0]])
 n = len(E)
-k = len(E2[0])
-m = len(E2)
+k = len(E[0])
 
-# Given the existence of A an m x n matrix and B an n x p matrix
-# The following algorithm is used to multiply A x B
+E2 = np.zeros((n,k), dtype=int)
 
-# To begin, we need to initialize the resultant matrix to zeros
-C = np.zeros((n,k), dtype=int)
-
-# The algorithm to do the matrix multiplication follows
 for i in range(0,len(E)) :
-    for j in range(0,len(E2[0])) :
-        C[i][j] = 0
-        for k in range(0,len(E2)) :
-            if (E[i][j] == 1 and E[j][i] == 1):
-                C[i][j] += E[i][k] * E[k][j]
+    for j in range(i+1,len(E[0])) :
+        E2[i][j] = 0
+        if (E[i][j] == 1 and E[j][i] == 1):
+            E2[i][j] = E[i][j] 
+            E2[j][i] = E[j][i]
 
-print(C)
+print(E2)
 
 
