5084eefd80ae4ee5356b4f9f7f3380b282776d8b
Gary Crossland <garycr@microsoft.com>
2021-04-11 14:55:56 -0500

    solution squaring adjacency matrix E

--- Pbm1.py
+++ Pbm1.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-A = ([[0,1,0],[0,0,1],[1,1,0]])
-B = ([[0,1,0],[0,0,1],[1,1,0]])
+E = ([[0,1,0],[0,0,1],[1,1,0]])
+E2 = ([[0,1,0],[0,0,1],[1,1,0]])
 
-n = len(A)
-k = len(B[0])
-m = len(B)
+n = len(E)
+k = len(E2[0])
+m = len(E2)
 
 # Given the existence of A an m x n matrix and B an n x p matrix
 # The following algorithm is used to multiply A x B
@@ -14,12 +14,12 @@ m = len(B)
 C = np.zeros((n,k), dtype=int)
 
 # The algorithm to do the matrix multiplication follows
-for i in range(0,len(A)) :
-    for j in range(0,len(B[0])) :
+for i in range(0,len(E)) :
+    for j in range(0,len(E2[0])) :
         C[i][j] = 0
-        for k in range(0,len(B)) :
-            if (A[i][j] == 1 and B[j][i] == 1):
-                C[i][j] += A[i][k] * B[k][j]
+        for k in range(0,len(E2)) :
+            if (E[i][j] == 1 and E[j][i] == 1):
+                C[i][j] += E[i][k] * E[k][j]
 
 print(C)
 
