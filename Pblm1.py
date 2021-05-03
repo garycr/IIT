b18de4482a5d5812d2eed9642e6d1d235b5aa7ee
Gary Crossland <garycr@microsoft.com>
2021-05-03 11:45:08 -0500

    Code complete with vertex and edges

--- Pblm1.py
+++ Pblm1.py
@@ -21,52 +21,83 @@ class Vertex:
 class Edge:
 	
 	# Constructor to create a new vertex
-	def __init__(self, vertex, weight):
-            self.vertex = vertex 
+	def __init__(self, u, v, weight):
+            self.u = u 
+            self.v = v
             self.weight = weight
+            self.color = 'White'
+
+def min(V, E, idx):
+    
+    min = int(0xFFFFFF)
+    e = -1
+
+    for i in range(0,len(E)):
+        if (V[E[i].u].color == 'Black' and E[i].color == 'White' and E[i].weight < min and V[E[i].v].color != 'Black'):
+            e = E[i]
+            min = e.weight
+    if (e != -1):
+        e.color = 'Grey'
+    return e
+
 
 # G=Graph, W=Weight, sV =Source Vertex
 def MODIFIED_PRIM(G,sV):
     # Graph, vertices, edges, and weights initialized during construction
-
-    min = int(0xFFFFFF)
-    idx = -1
-    for i in range(0, len(G.Adj[sV.index])): 
-        e = G.Adj[sV.index][i]
-        if (e.vertex.color = 'White' and e.weight < min ):
-            min = e.weight
-            idx = i
-
-    sV.color = 'Black'
-    sV.distance = min
-    sV = G.Adj[idx].vertex 
-
-
-
-
-
-
-        v = G.V[G.Adj[u.index][j].vertex]
-        alt = u.distance + G.Adj[u.index][j].weight
-        if (alt <= v.distance):
-            v.distance = alt
-            v.parent.append(u)
-            DecreaseKey(Q, v.index, v)
-
-
+    idx = sV.index
+    G.V[idx].color = 'Black'
+    e = min(G.V, G.Adj, idx)
+ 
+    while (e != -1):
+        G.V[e.v].color = 'Black'
+        G.V[e.v].distance = G.V[e.v].distance + e.weight
+        e = min(G.V, G.Adj, e.v)
     return 
 
+V = [Vertex(0),Vertex(1),Vertex(2),Vertex(3),Vertex(4),Vertex(5),Vertex(6),Vertex(7),Vertex(8)]
 
-V = [Vertex(0),Vertex(1),Vertex(2),Vertex(3),Vertex(4),Vertex(5)]
+E = [Edge(0,1,4),
+     Edge(1,0,4),
+     Edge(1,2,8),
+     Edge(2,1,8),
+     Edge(0,7,8),
+     Edge(7,0,8),
+     Edge(2,3,7),
+     Edge(3,2,7),
+     Edge(3,4,9),
+     Edge(4,3,9),
+     Edge(4,5,10),
+     Edge(5,4,10),
+     Edge(5,6,2),
+     Edge(6,5,2),
+     Edge(6,7,1),
+     Edge(7,6,1),
+     Edge(2,8,2),
+     Edge(8,2,2),
+     Edge(2,5,4),
+     Edge(5,2,4),
+     Edge(3,5,14),
+     Edge(5,3,14),
+     Edge(6,8,6),
+     Edge(8,6,6),
+     Edge(7,8,7),
+     Edge(8,7,7)
+    ]
+
+
+'''
 E = [
-    [Edge(1,1),Edge(2,7)],
-    [Edge(3,9),Edge(5,15)],
-    [Edge(4,4)],
-    [Edge(5,5),Edge(4,1)],
-    [Edge(5,3)],
-    [],
+    [Edge(1,4),Edge(7,8)],
+    [Edge(2,8)],
+    [Edge(3,7),Edge(5,4),Edge(8,2)],
+    [Edge(4,9),Edge(5,14)],
+    [Edge(5,10)],
+    [Edge(3,14),Edge(6,2)],
+    [Edge(7,1),Edge(8,6)],
+    [Edge(0,8),Edge(8,7)],
+    [Edge(2,2),Edge(7,7),Edge(6,6)],
    ]
+'''
 
 G = Graph(V,E)
-
 MODIFIED_PRIM(G, V[0])
